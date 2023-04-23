c24047dea6d36bc9077362689241e05e7e87aeca
Jean Lucca <jefreecs.silva@gmail.com>
2023-04-23 20:26:08 -0300

    fix the code format (prettier)

--- server.js
+++ server.js
@@ -1,21 +1,19 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser') 
-app.use(bodyParser.json())
-const http = require ('http')
-const PORT = 5000
-const router = express.Router()
-const pokemonRouter = require('./routers/pokemon')
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+const http = require("http");
+const PORT = 5000;
+const router = express.Router();
+const pokemonRouter = require("./routers/pokemon");
 
 app.listen(PORT, () => {
-    console.log(`using the PORT: ${PORT}`)
-})
+  console.log(`using the PORT: ${PORT}`);
+});
 
 //pokedex
-app.get('/', (req,res)=> {
-    res.status(200).send("Pokedex principal page")
-})
-app.use('/pokemons', pokemonRouter)
-app.use(express.json())
-
-
+app.get("/", (req, res) => {
+  res.status(200).send("Pokedex principal page");
+});
+app.use("/pokemons", pokemonRouter);
+app.use(express.json());
