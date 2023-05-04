d015405f938ff175d0720d11004bd49855c6b6b2
Jean Lucca <jefreecs.silva@gmail.com>
2023-05-03 22:30:06 -0300

    add communication with html and API

---
+++ pokedex_jnx/getpokedex_prime.js
@@ -0,0 +1,29 @@
+function getPoke() {
+    fetch('http://localhost:5000/pokemons')
+      .then(response => response.json())
+      .then(data => {
+        const dataList = document.querySelector('#pokemon-list listbody')
+        dataList.innerHTML = ''
+        data.forEach(element => {
+          const row = document.createElement('li')
+          const nameElement = document.createElement('li')
+          nameElement.textContent = element.name
+          const idElement = document.createElement('li')
+          idElement.textContent = element.id
+          const typeElement = document.createElement('li')
+          typeElement.textContent = element.type
+          const imageElement = document.createElement('li')
+          const image = document.createElement('img')
+          image.src = element.image
+          image.alt = element.name
+          imageElement.appendChild(image)
+          row.appendChild(nameElement)
+          row.appendChild(idElement)
+          row.appendChild(typeElement)
+          row.appendChild(imageElement)
+          dataList.appendChild(row)
+        })
+      })
+      .catch(error => console.log(error))
+  }
+  getPoke()
